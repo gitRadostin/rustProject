2398c105829ee1dfafbdc06331866e6a5aa37ac8
gitRadostin <radiilievx@gmail.com>
2025-03-19 17:15:03 +0200

    Add variable types - float, bool, char

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-// Rust has signed (positive/negative) and unsigned (only positive) integer types of different sizes.
-//
-// i8, i16, i32, i64, i128:  Signed integers.
-// u8, u16, u32, u64, u128:  Unigned integers.
+
 
 fn main() {
+    // Rust has signed (positive/negative) and unsigned (only positive) integer types of different sizes.
+    // i8, i16, i32, i64, i128:  Signed integers.
+    // u8, u16, u32, u64, u128:  Unigned integers.
     let x: i64 = -42;
     let y: u64 = 100;
 
-    println!("Signed integer    :  {}", x);
-    println!("Unsigned integer  :  {}", y);
+    println!("Signed integer   : {}", x);
+    println!("Unsigned integer : {}", y);
+
+    // Float - f32, f64
+    let pi: f64 = 3.1415926535897932384626433832795;
+    println!("Pi               : {}", pi);
+
+    // Bool (boolean) - true, false
+    let is_running: bool = true;
+    println!("Is running       : {}", is_running);
+
+    // Char (character) - char
+    let letter: char = 'A';
+    println!("Letter           : {}", letter);
 }
