7eda54383ee9e86fbd57cb39c3e0579dd6c6dd5d
gitRadostin <radiilievx@gmail.com>
2025-03-19 16:57:56 +0200

    Added integer types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// Rust has signed (positive/negative) and unsigned (only positive) integer types of different sizes.
+//
+// i8, i16, i32, i64, i128:  Signed integers.
+// u8, u16, u32, u64, u128:  Unigned integers.
+
 fn main() {
-    println!("Hello, World!");
+    let x: i64 = -42;
+    let y: u64 = 100;
+
+    println!("Signed integer    :  {}", x);
+    println!("Unsigned integer  :  {}", y);
 }
